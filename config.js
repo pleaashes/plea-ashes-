26674fd4219ebb557d66a155092a0849f2bd673d
pleaashes <pleaashes0@gmail.com>
2026-09-22 01:30:15 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase configuration used by app.js.
+// This file is optional; app.js already contains the values for this project.
+window.PLEA_ASHES_SUPABASE = {
+  url: "https://euiadxlpqnvrbyxrzelp.supabase.co",
+  publishableKey: "sb_publishable_LrIwp7KKrOy_ha_gDeHv8g_zGeTMCND"
+};
